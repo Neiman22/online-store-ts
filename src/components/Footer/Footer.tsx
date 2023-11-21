246f71afe900e59856d4400bf4c570330a98fbde
Neiman22 <pushkarev_av@inbox.ru>
2023-11-21 15:09:08 +0300

    fix: add Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,45 @@
-const Footer = () => {
-  return (
-    <div className="app">
-      Footer
-    </div>
-  )
-}
+import { Link } from 'react-router-dom';
 
-export default Footer
+import styles from '../../styles/Footer.module.css';
+
+import { ROUTES } from '../../utils/routes';
+import LOGO from '../../images/logo.svg';
+
+const Footer = () => (
+  <section className={styles.footer}>
+    <div className={styles.logo}>
+      <Link to={ROUTES.HOME}>
+        <img src={LOGO} alt="Stuff" />
+      </Link>
+    </div>
+
+    <div className={styles.rights}>
+      Developed by{" "}
+      <a href="https://github.com/Neiman22" target="_blank" rel="noreferrer">
+        Pushkarev
+      </a>
+    </div>
+
+    <div className={styles.socials}>
+      <a href="https://instagram.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+        </svg>
+      </a>
+
+      <a href="https://facebook.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+        </svg>
+      </a>
+
+      <a href="https://youtube.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+        </svg>
+      </a>
+    </div>
+  </section>
+);
+
+export default Footer;
