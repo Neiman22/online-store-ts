cfc0960046cf7bc97f5d9c55ef5d4718ffc7282a
Neiman22 <pushkarev_av@inbox.ru>
2023-11-21 16:25:31 +0300

    fix: add Sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,33 @@
+import { NavLink } from "react-router-dom";
+
+import styles from "../../styles/Sidebar.module.css";
+
 const Sidebar = () => {
   return (
-    <div className="app">
-      Sidebar
-    </div>
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`/categories/${1}`}>Category</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <a href="/help" target="_blank" className={styles.link}>
+          Help
+        </a>
+        <a
+          href="/terms"
+          target="_blank"
+          className={styles.link}
+          style={{ textDecoration: "underline" }}
+        >
+          Terms & Conditions
+        </a>
+      </div>
+    </section>
   )
 }
 
